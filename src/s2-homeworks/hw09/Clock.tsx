658da4776e2669c33a55acd4bf4ba8e31cf8fbec
YuldashevToir <reindeerinthedarkforest@gmail.com>
2025-07-08 17:19:12 +0500

    HW9.5

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -44,7 +44,7 @@ function Clock() {
         second: '2-digit',
         hour12: false
       })  || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString("en-US", { year: 'numeric', month: '2-digit' , day:'2-digit' }) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = date.toLocaleDateString("ru-RU", { year: 'numeric', month: '2-digit' , day:'2-digit' }) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = new Intl.DateTimeFormat('en-US', { weekday: "long" }).format(date);
